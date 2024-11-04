eb0b54d2141248cd658ef3fbb5f18e0e91b83d90
arslanPR16 <khasan8983@gmail.com>
2024-11-04 20:49:39 +0500

    first commit

---
+++ lesson_1_number.py
@@ -0,0 +1,92 @@
+num1=True
+num2=False
+num_1=4
+if(num_1==4):
+        print("true")
+else:
+        print("False")
+
+
+
+num4=1
+num5=4
+if(num4<=1 or num5<=1):
+       print("num4<=1 or num5!=1")
+else:
+       print("num4!=1 or num5<=1")
+
+
+
+num6=555
+if(num6>0):
+       print("num6>0")
+if(num6<0):
+       print("num6<0")
+if(num6==0):
+       print("num6==0")
+
+
+
+num1=5
+num2=3
+num3=3
+num4=3
+num5=3
+num6=((num1+num2+num3+num4+num5)/5)
+if(num6>=3):
+           print("num6>=3")
+if(num6<3):
+           print("num6<3")
+ 
+
+
+
+
+num10=7
+if(num10/2):
+         print("num10*3")
+if(num10/2):
+    print("num10/2")
+else:
+    print("Fales")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
